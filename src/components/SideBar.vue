265ab14edb51876508f0e93c8bc93d03e44b7141
Nigel <45549642+Nigel-wei-zhe@users.noreply.github.com>
2020-11-08 15:25:02 +0800

    [add] quasar init & test page

---
+++ src/components/SideBar.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <q-item
+      v-for="(item, itemIndex) in sideBarList"
+      :key="`sideBarList-${itemIndex}`"
+      clickable
+      tag="a"
+      target="_blank"
+      @click="goto(item.link)"
+    >
+      <q-item-section v-if="item.icon" avatar>
+        <q-icon :name="item.icon" />
+      </q-item-section>
+
+      <q-item-section>
+        <q-item-label>{{ item.title }}</q-item-label>
+        <q-item-label caption>
+          {{ item.caption }}
+        </q-item-label>
+      </q-item-section>
+    </q-item>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SideBar",
+  data() {
+    return {
+      sideBarList: [
+        {
+          title: "記帳表單",
+          caption: "送出表單,統計花費",
+          link: "/123",
+          icon: "request_quote"
+        }
+      ]
+    };
+  },
+  methods: {
+    goto(link) {
+      this.$router.push(link);
+    }
+  }
+};
+</script>
---
+++ src/pages/aaa.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page class="flex flex-center">
+    aaa
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "PageIndex"
+};
+</script>
